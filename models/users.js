b2cec433a1603a52039f17451fdcaba183a9e2f3
Basile Favretto <bazil.f@gmail.com>
2024-06-07 10:35:38 +0200

    Authentication routes

--- models/users.js
+++ models/users.js
@@ -1,40 +1,44 @@
 const mongoose = require("mongoose");
 
-const UserSchema = mongoose.Schema({
-    username: {
-        type: String,
-        required: true,
-        unique: true,
-        trim: true,
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-        select: false,
-        trim: true,
-        validate: {
-            validator: (val) =>
-                /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
-                    val
-                ),
-            message: ({ value }) => `${value} is not a valid email.`,
+const UserSchema = mongoose.Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            select: false,
+            trim: true,
+            validate: {
+                validator: (val) =>
+                    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+                        val
+                    ),
+                message: ({ value }) => `${value} is not a valid email.`,
+            },
+        },
+        password: {
+            type: String,
+            select: false,
+            // this = document
+            // required: () => this.status !== "pending",
+        },
+        type: {
+            type: String,
+            enum: ["admin", "reseller", "user"],
+            default: "user",
+            required: true,
         },
     },
-    password: {
-        type: String,
-        select: false,
-        // this = document
-        required: () => this.status !== "pending",
-    },
-    type: {
-        type: String,
-        enum: ["admin", "reseller", "user"],
-        default: "user",
-        required: true,
-    },
-    timestamps: true,
-});
+    {
+        timestamps: true,
+    }
+);
 
 // UserSchema.post("deleteOne", async function () {
 //     // this = request / query
@@ -45,6 +49,6 @@ const UserSchema = mongoose.Schema({
 //     await Comment.deleteMany({ user: userId });
 // });
 
-const User = mongoose.model("users", userSchema);
+const User = mongoose.model("users", UserSchema);
 
 module.exports = User;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,40 @@
-var express = require('express');
+var express = require("express");
+const User = require("../models/users");
 var router = express.Router();
+const bcrypt = require("bcrypt");
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function (req, res, next) {
+    res.send("respond with a resource");
+});
+
+router.post("/login", async (req, res) => {
+    const { email, password } = req.body;
+    console.log(req.body);
+    const user = await User.findOne({ email: email }).select("password").select("email");
+    console.log("user", user);
+    if (!user || !bcrypt.compareSync(password, user.password)) {
+        return res.sendStatus(401);
+    }
+    return res.json({ data: { result: true, user: user } });
+});
+
+router.post("/register", async (req, res) => {
+    const { username, email, password } = req.body;
+    const user = await User.findOne({ email: email });
+    if (user) {
+        return res.sendStatus(409);
+    }
+
+    const newUser = new User({
+        username,
+        email,
+        password: bcrypt.hashSync(password, 10),
+    });
+
+    const createdUser = await newUser.save();
+
+    return res.json({ data: { result: true, user: createdUser } });
 });
 
 module.exports = router;
