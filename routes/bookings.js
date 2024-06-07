181a6a8601a1aff470c24643dd02891adf96721b
Basile Favretto <bazil.f@gmail.com>
2024-06-07 14:25:38 +0200

    Booking creation and index

--- routes/bookings.js
+++ routes/bookings.js
@@ -1,26 +1,41 @@
 var express = require("express");
 const Bike = require("../models/bikes");
+const User = require("../models/users");
 const Booking = require("../models/bookings");
 var router = express.Router();
 
 router.get("/", async (req, res) => {
-    const bookings = await Booking.find();
+    const bookings = await Booking.find()
+        .populate({
+            path: "user",
+            model: User,
+        })
+        .populate({
+            path: "bikes",
+            model: Bike,
+        });
+    // .populate({ path: "user", select: "username", select: "email" });
+    console.log(bookings);
     return res.json({ result: true, bookings });
 });
 
 router.post("/", async (req, res) => {
-    const { bikes, startAt, endAt } = req.body;
-    const bookedBikes = await Bike.find({ _id: { $in: bikes } });
-
+    const { bikes, startAt, endAt, user } = req.body;
+    // const bookedBikes = await Bike.find({ _id: { $in: bikes }, status: "available" });
+    console.log(req.body);
     const newBooking = new Booking({
         bikes,
         startAt,
         endAt,
+        user,
     });
 
     const savedBooking = await newBooking.save();
-    await bookedBikes.update({ status: "reserved" });
-    console.log(req.body, newBooking);
+    const bookedBikes = await Bike.updateMany(
+        { _id: { $in: bikes }, status: "available" },
+        { $set: { status: "reserved" } }
+    );
+    console.log(req.body, newBooking, bookedBikes);
     return res.json({ data: { result: true, booking: savedBooking.populate("bikes") } });
 });
 
